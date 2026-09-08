06d0027a0e16d6744dc98f3de6de989c04596e42
solomon <solomanakpan2011@gmail.com>
2026-09-08 06:26:01 -0400

    Set up SchoolConnect frontend

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const studentsRouter = require("./routes/students");
 const teachersRouter = require("./routes/teachers");
 const classesRouter = require("./routes/classes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "School Management System API is running!"
-    });
-});
-
+// API routes
 app.use("/api/students", studentsRouter);
 app.use("/api/teachers", teachersRouter);
 app.use("/api/classes", classesRouter);
 
+// Serve frontend
+app.use(express.static(path.join(__dirname, "frontend")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`SchoolConnect server running on port ${PORT}`);
 });
